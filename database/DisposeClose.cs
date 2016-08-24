f6afbf9f93e0041e95e99cd99e7a9b34054604a7
lbw <lbwtorino@gmail.com>
2016-08-24 21:20:31 +0800

    cs

---
+++ database/DisposeClose.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace MMAWPF
+{
+   class DisposeClose
+   {
+      public static void Disposeclose(SqlConnection connection)
+      {
+         connection.Dispose();
+         connection.Close();
+      }
+      public static void Disposeclose(SqlDataReader dreader)
+      {
+         dreader.Dispose();
+         dreader.Close();
+      }
+      public static void Disposeclose(SqlDataAdapter dadapter)
+      {
+         dadapter.Dispose();
+      }
+      public static void Disposeclose(SqlCommand command)
+      {
+         command.Dispose();
+      }
+   }
+}
