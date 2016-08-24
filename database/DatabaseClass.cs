2d3b05f160be334b6f4e573d78a8a318e8821dcd
lbw <lbwtorino@gmail.com>
2016-08-24 21:19:40 +0800

    cs

---
+++ database/DatabaseClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace MMAWPF
+{
+   class DatabaseClass
+   {
+      public static SqlConnection Conn { get; set; }
+      public static string Constr { get; set; }
+      public static SqlCommand ConnectionToCommad(SqlConnection con, string constr)
+      {
+         Conn = con;
+         Constr = constr;
+         Conn = new SqlConnection(Constr);
+         try
+         {
+            Conn.Open();
+            SqlCommand com = new SqlCommand("", Conn);
+            return com;
+         }
+         catch (Exception e)
+         {
+            MessageBox.Show("数据库连接失败！");
+            return null;
+         }
+
+      }
+   }
+}
