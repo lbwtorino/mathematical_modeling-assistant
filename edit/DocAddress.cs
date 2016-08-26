d2dc406ed8bcf8bd298214f17b1a708b71232567
lbw <lbwtorino@gmail.com>
2016-08-26 11:04:20 +0800

    cs

---
+++ edit/DocAddress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMAWPF.文档编辑模块
+{
+   class DocAddress
+   {
+      public static string DocPath { get; set; }
+   }
+}
