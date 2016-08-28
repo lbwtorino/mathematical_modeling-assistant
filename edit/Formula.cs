53ea22f103637a61b8877e971d5a9fed902d24d3
lbw <lbwtorino@gmail.com>
2016-08-28 20:23:11 +0800

    formula

---
+++ edit/Formula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMAWPF.文档编辑模块
+{
+   class Formula
+   {
+      public static int FNumber { get; set; }
+      public static string FName { get; set; }
+      public static string FTag { get; set; }
+      public static bool isChanged;
+      public static string ModFName { get; set; }
+   }
+}
