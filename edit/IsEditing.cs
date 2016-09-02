7d1cedec53de690eb29965e36f3aa67b0a3f9ed8
lbw <lbwtorino@gmail.com>
2016-09-02 15:38:01 +0800

    xaml

---
+++ edit/IsEditing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMAWPF.文档编辑模块
+{
+   class IsEditing
+   {
+      public static string ElementName { get; set; }
+      public static int DOCID { get; set; }
+   }
+}
