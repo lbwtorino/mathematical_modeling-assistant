83c534e4a67273c88f5e9f2bf32c686b8dd7d9ed
lbw <lbwtorino@gmail.com>
2016-09-07 17:22:38 +0800

    load

---
+++ edit/Picture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMAWPF.文档编辑模块
+{
+   class Picture
+   {
+      public static bool isChanged;
+      private static string picTag;
+      private static string picName;
+      private static string picAddress;
+      public static int PNum { get; set; }
+      public static string PicTag
+      {
+         get
+         {
+            return picTag;
+         }
+         set
+         {
+            picTag = value;
+         }
+      }
+      public static string PicName
+      {
+         get
+         {
+            return picName;
+         }
+         set
+         {
+            picName = value;
+         }
+      }
+      public static string PicAddress
+      {
+         get
+         {
+            return picAddress;
+         }
+         set
+         {
+            picAddress = value;
+         }
+      }
+   }
+}
