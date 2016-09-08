f466660908a7d215d0cc2e38a1bac3d206aba4c0
lbw <lbwtorino@gmail.com>
2016-09-08 17:06:26 +0800

    table

---
+++ edit/Table.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMAWPF.文档编辑模块
+{
+   class Table
+   {
+      public static bool isChanged;
+      private static string tableTag;
+      private static string tableName;
+      private static int rowNum;
+      private static int colNum;
+      public static int TNum { get; set; }
+      public static string TableTag
+      {
+         get
+         {
+            return tableTag;
+         }
+         set
+         {
+            tableTag = value;
+         }
+      }
+      public static string TableName
+      {
+         get
+         {
+            return tableName;
+         }
+         set
+         {
+            tableName = value;
+         }
+      }
+      public static int RowNum
+      {
+         get
+         {
+            return rowNum;
+         }
+         set
+         {
+            rowNum = value;
+         }
+      }
+      public static int ColNum
+      {
+         get
+         {
+            return colNum;
+         }
+         set
+         {
+            colNum = value;
+         }
+      }
+   }
+}
