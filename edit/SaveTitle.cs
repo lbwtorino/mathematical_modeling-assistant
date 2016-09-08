b2a686c434b05f7292c2f093b49ce4cfad9c2493
lbw <lbwtorino@gmail.com>
2016-09-08 17:05:43 +0800

    save

---
+++ edit/SaveTitle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMAWPF.文档编辑模块
+{
+   class SaveTitle
+   {
+      private static string titleName = "";
+      public static string TitleName
+      {
+         get
+         {
+            return titleName;
+         }
+         set
+         {
+            titleName = value;
+         }
+      }
+   }
+}
