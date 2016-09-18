7a4d9fee95ddc5ee77ec908efdab7bd03de5d3d5
lbw <lbwtorino@gmail.com>
2016-09-18 10:40:08 +0800

    login

---
+++ login/UserLogIn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MMAWPF
+{
+   class UserLogIn
+   {
+      private static string userName="";
+      private static DateTime editTime;
+      public static string UserName
+      {
+         get
+         {
+            return userName;
+         }
+         set
+         {
+            userName = value;
+         }
+      }
+      public static DateTime EditTime
+      {
+         get
+         {
+            return editTime;
+         }
+         set
+         {
+            editTime = value;
+         }
+      }
+   }
+}
